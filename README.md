b6398eca950056f74655f573398d849a1dae2cd7
Hiryus <hiryus.sha@gmail.com>
2025-07-10 17:30:22 +0200

    improve documentation

--- README.md
+++ README.md
@@ -3,9 +3,79 @@
 This tool analyzes EvE Online chat logs and looks for entosis reports with dedicated format.
 It then shows a synthetic view of on-going entosis.
 
-![Running screenshot](https://s21.postimg.org/oab4ekh3r/entosis_helper.png)
+![Running screenshot](doc/screenshot.png)
+
+## Usage
+
+By default, the tool parses fleet chat (it needs to be started after the fleet creation or it will load last fleet logs !). You can also use another file by clicking the "↺" button (top left corner of the window).
+
+It recognize the following format : `[system_name] / [node_id] / [status] / [time_left]`, for example:
+* `DO6H-Q / Z99 / friendly / 10:00`
+* `ZAU-JW / P21 / enemy / 0:55`
+* `1M4-FK / A01 / neutral / none`
+
+### System
+
+The `[system]` is obviously the name of the system where the node is located (reminder: all nodes spawn in the same constellation).
+
+### Node id
+
+The `[node_id]` is the last part of a command node name which helps differentiating several ndoes in the same system.  
+To be clear, all nodes are named with the following convention: `[system] Infrastructure Hub Command Node [node_id]`, where `[system]` is the name of the system for which the entosis event is running (⚠️ it is _not_ the node id) and the very last part of the name is the actual node id. It always starts with a letter followed by two digits for a total of three characters.  
+So, for example, in the name `ZAU-JW Infrastructure Hub Command Node B58`, the node id is `B58`.
+
+### Status
+
+`[status]` can be one of the following values :
+* `new` - node just spawned,
+* `friendly` - an ally is entosissing,
+* `enemy` - an enemy is entosissing,
+* `neutral` - nobody is entosissing,
+* `unknown` - you don't know if someone is entosissing.
+Note that "new", "neutral" and "unknown" are considered the same and are shown as "neutral". You can use either command as you prefer.
+
+### Timer
+
+`[time_left]` must follow a strict format : `[minutes]:[seconds]`.  
+* ... but you can also use the `warmup` keyword instead of a timer.
+* ... or the `done` keyword to indicate the node is gone.
+* ... and for neutral nodes, use the `none` keyword (or the other too will work too - there is no timer for neutral nodes anyway).
+
+## Tips and tricks
+
+I highly recommand you to **drag and drop the system name** instead of writting it manually.
+Even if the application tries to infer your input in case of typo, it is not really reliable. Drag and drop will be faster than wrtting the full name anyway.
+
+The tool selects the last fleet file by default. If you have several characters logged-in in different fleets, make sure to double-check as Murphy law says that you will never get the right one !
+
+You can click on a system name to show or hide nodes details.
+
+Commands are not case sensitive.
 
 ## Contact
 
 - Ryanis (in game name)
 - ryanis.shadow@gmail.com (email)
+
+## For developers
+
+You need a modern version of [nodejs](https://nodejs.org) to build the application.
+
+It uses [Electron](https://www.electronjs.org/) and [VueJS](https://vuejs.org/) under the hood.  
+You may want to familiarize with their documentations and especially the tutorials since they are not excatly _straightforward_.
+
+Install dependencies first:
+```bash
+npm install
+```
+
+To run a local development build (where renderer changes are updated live, but not the main process code):
+```bash
+npm start
+```
+
+To Package the application into a deliverable ZIP file:
+```bash
+npm run make
+```
+NB: the `npm run package` command is similar, except it builds a portable version of the applicaiton without bundling it into a ZUP file.
--- src/view/help.vue
+++ src/view/help.vue
@@ -15,10 +15,28 @@
 
         <h3>Commands</h3>
         <p>
-            It recognize the following <em>format</em> :
-            <span class="code">[system_name] / [node_id] / [status] / [time_left]</span>
+            It recognize the following <em>format</em>:
             <br>
-            Example : <span class="code">DO6H-Q / Z99 / friendly / 0:55</span>
+            <span class="code">[system] / [node_id] / [status] / [time_left]</span>
+        </p>
+        <p>
+            Examples:
+        </p>
+        <ul>
+            <li><span class="code">DO6H-Q / Z99 / friendly / 10:00</span></li>
+            <li><span class="code">ZAU-JW / P21 / enemy / 0:55</span></li>
+            <li><span class="code">1M4-FK / A01 / neutral / none</span></li>
+        </ul>
+        <p>
+            The <span class="code">[system]</span> is obviously the name of the system where the node is located (reminder: all nodes spawn in the same constellation).
+        </p>
+        <p>
+            <span class="code">[node_id]</span> is the <em>node identifier</em> appearing in the last part of a command node name. It helps differentiating several nodes in a same system.
+            <br>
+            To be clear, all nodes are named with the following convention:
+            <br><span class="code">[system] Infrastructure Hub Command Node [node_id]</span>, where <span class="code">[system]</span> is the name of the system for which the entosis event is running (⚠️ it is <u>not</u> the node id) and the very last part of the name is the actual node id. It always starts with a letter followed by two digits for a total of three characters.
+            <br>
+            So, for example, in the name <span class="code">ZAU-JW Infrastructure Hub Command Node B58</span>, the node id is <span class="code">B58</span>.
         </p>
         <p>
             <span class="code">[status]</span> can be one of the following values :
@@ -31,28 +49,31 @@
             <li><span class="code">unknown</span> - you don't know if someone is entosissing.</li>
         </ul>
         <p>
-            Note that "new", "neutral" and "unknown" are considered the same and are shown as "neutral". You can use either command as you prefer.
+            Note that "new", "neutral" and "unknown" are considered the same and are shown as "neutral". You can use either keyword as you prefer.
         </p>
         <p>
-            <span class="code">[time_left]</span> must follow a strict format : <span class="code">[minutes]:[seconds]</span>.
+            <span class="code">[time_left]</span> is the <em>remaining time</em> before entosis finishes.
+            <br>
+            It must follow a strict format for the time: <span class="code">[minutes]:[seconds]</span>.
         </p>
         <ul>
             <li>... but you can also use the <span class="code">warmup</span> keyword instead of a timer.</li>
             <li>... or the <span class="code">done</span> keyword to indicate the node is gone.</li>
+            <li>... and for neutral nodes, you can use the <span class="code">none</span> keyword (or any valid format actually - there is no timer for neutral nodes anyway).</li>
         </ul>
 
         <h3>Tips and tricks</h3>
         <p>
-            You can <em>click on a system</em> name to show or hide nodes details.
+            I highly recommand you to <em>drag and drop</em> system name instead of writting it manually.
+            Even if the application tries to infer your input in case of typo, it is not really reliable.
+            Drag and drop will be faster than wrtting the full name anyway.
         </p>
         <p>
             The tool selects the last fleet file by default.
             If you have <em>several characters</em> logged-in in different fleets, make sure to double-check as Murphy law says that you will never get the right one !
         </p>
         <p>
-            I highly recommand you to <em>drag and drop</em> system name instead of writting it manually.
-            Even if the application tries to infer your input in case of typo, it is not really reliable.
-            Drag and drop will be faster than wrtting the full name anyway.
+            You can <em>click on a system</em> name to show or hide nodes details.
         </p>
         <p>
             Commands are <em>not</em> case sensitive.
